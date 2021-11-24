68ccd687c7ef6e9f277997df7e5baa05cf257f62
Your Name <you@example.com>
2021-11-24 16:21:03 -0300

    lista-prestador-detalhes + duvidas

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      telaAtual: 'Home',
+      telaAtual: '+ Detalhes Serviço',
       Carrinho: []
     }
     this.irHome = this.irHome.bind(this)
